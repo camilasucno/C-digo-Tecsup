bac262878a1ec746625846f11ec942564f7f1620
Camila Sucno <camilasucno22@gmail.com>
2022-04-07 20:30:11 -0500

    update

---
+++ Semana6/Dia2/02-document.js
@@ -0,0 +1,39 @@
+/**
+ Document es un objeto que me va a permitir modificar el DOM
+*/
+//Quiero obtener un elem por su ID
+let titulo = document.getElementById("titulo")
+
+console.log("Revisando Elem", titulo)
+
+//innerHTML es una propiedad que me permite cambiar el HTML interno de un elem
+titulo.innerHTML = "<i>Nuevo Título</i>"
+
+//nomenclatura Varibles
+//elem.style.propiedad = "valorPropiedadCSS"
+//snake-case --> camelCase
+titulo.style.color = "red";
+titulo.style.padding = "20px";
+titulo.style.backgroundColor= "blue";
+titulo.style.borderRadius = "10px";
+titulo.style.textAlign = "center";
+
+//Selector CSS
+let subtitulo = document.querySelector(".subtitulo")
+
+console.log(subtitulo);
+
+subtitulo.style.border = "4px solid #4c4c4c";
+
+let main = document.querySelector("main");
+
+main.style.backgroundColor = "lightblue";
+
+let listaItems = document.querySelector(".li_item")
+
+console.log(listaItems)
+
+listaItems.forEach(function(liItem){
+    liItem.style.fontFamily = "Arial";
+    liItem.style.color = "purple"
+})
